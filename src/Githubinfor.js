28542fd232dfcf53b9d24f2fc930fb4b11024b35
Your Name <you@example.com>
2021-05-11 14:16:34 +0800

    'firt'

---
+++ src/Githubinfor.js
@@ -0,0 +1,50 @@
+import React from 'react';
+import $ from 'jquery';
+
+
+class UserGithub extends React.Component {
+    constructor(props) {
+    super(props);
+    this.state = {
+    username: '',
+    githubtUrl: '',
+    avatarUrl: '',
+    locate: '',
+    public_repos: '',
+    followers: '',
+    }
+    }
+    componentDidMount() {
+    $.get(this.props.source, (result) => {
+    console.log(result);
+    const data = result;
+    if (data) {
+    this.setState({
+    username: data.name,
+    githubtUrl: data.html_url,
+    avatarUrl: data.avatar_url,
+    locate: data.location,
+    public_repos: data.public_repos,
+    followers: data.followers,
+    });
+    }
+    });
+    }
+    render() {
+    return (
+    <div>
+    <img src={this.state.avatarUrl} /><br/>
+    <h3>user name:{this.state.username}</h3>
+    <a href={this.state.githubtUrl}>Github Link</a>
+    <h3>location:{this.state.locate}</h3>
+    <h3>public_repos:{this.state.public_repos}</h3>
+    <h3>followers:{this.state.followers}</h3>
+    </div>
+    );
+    }
+   }
+//    ReactDOM.render(
+//     <UserGithub source="https://api.github.com/users/cjwu" />,
+//     document.getElementById('root')
+//    );
+   export default UserGithub;
